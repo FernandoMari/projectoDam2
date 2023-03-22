e1c6532b9c483d4315ae955ae5c72d8efd87e2f5
Fernando Ismael Mariduena Andrade <fema4736@proven.local>
2023-03-22 13:32:15 +0100

    Initial commit

---
+++ app/src/main/java/org/proven/decisions2/SocialInterface.java
@@ -0,0 +1,14 @@
+package org.proven.decisions2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SocialInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_interface);
+    }
+}
