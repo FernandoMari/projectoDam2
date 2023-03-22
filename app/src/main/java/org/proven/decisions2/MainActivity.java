fd95e9f6bbce60e78fec7420814ceb34d299d480
ismael <ismaelito11997@gmail.com>
2023-03-22 17:54:03 +0100

    commit del dia 22/03/2023

--- app/src/main/java/org/proven/decisions2/MainActivity.java
+++ app/src/main/java/org/proven/decisions2/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    /*Instantiate the variables*/
     EditText inputEmail, inputPassword;
     Button btLogin, btRegister;
     ProgressDialog progressDialog;
@@ -40,14 +43,21 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
 
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        /*The "btLogin" button uses a click event listener that is defined by an anonymous class, which implements the View.OnClickListener interface.
+         *The anonymous class has an onClick() method that is called when the button is clicked.
+         *Inside the onClick() method, the perforLogin() method is called, which has the login logic.*/
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 perforLogin();
             }
         });
-
+        /*The "btRegister" button  also uses a click listener that is defined by an anonymous class, which also implements the View.OnClickListener interface.
+         *The anonymous class has an onClick() method that is called when the button is clicked.
+         *Inside the onClick() method, an object of the Intent class is created and the current activity (MainActivity) and the register activity (Register) are passed as parameters.
+         *The startActivity() method is then called to start the registration activity.*/
         btRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,12 +66,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /*Method with the logic of logging into the application.
+     * The method gets the values entered for the email and password, and then validates the email to ensure it is in the proper format using an emailPattern regular expression.*/
     private void perforLogin() {
         String email = inputEmail.getText().toString();
         String password = inputPassword.getText().toString();
-
+        /*Check If the email does not match the pattern, an error message is displayed in the email input field.*/
         if (!email.matches(emailPattern)) {
             inputEmail.setError("Enter conntext Email");
+       /*Check if the password is valid. If the password is less than 6 characters or empty, an error message is displayed in the password input field.*/
         } else if (password.isEmpty() || password.length() < 6) {
             inputPassword.setError("Enter Proper Password");
         } else {
@@ -69,14 +82,15 @@ public class MainActivity extends AppCompatActivity {
             progressDialog.setTitle("Login");
             progressDialog.setCanceledOnTouchOutside(false);
             progressDialog.show();
+            /*The email and password are valid, a progress message is displayed and the signInWithEmailAndPassword() method is called to perform the sign-in using the mAuth object (presumably an instance of the FirebaseAuth class).*/
             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                /*The signInWithEmailAndPassword() method returns an asynchronous task that completes with an AuthResult authentication result. The addOnCompleteListener() method is used to add an event listener to this task that is called when the task completes.*/
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
                         progressDialog.dismiss();
                         sendUserToNextActivity();
                         Toast.makeText(MainActivity.this, "Login Succesful", Toast.LENGTH_SHORT).show();
-
                     } else {
                         progressDialog.dismiss();
                         Toast.makeText(MainActivity.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
@@ -89,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*Method to go to the social interface*/
     private void sendUserToNextActivity() {
         Intent intent = new Intent(MainActivity.this, SocialInterface.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
--- app/src/main/java/org/proven/decisions2/Register.java
+++ app/src/main/java/org/proven/decisions2/Register.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,9 +21,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
-    EditText inputEmail,inputPassword,inputConfirmPasword;
+    EditText inputEmail, inputPassword, inputConfirmPasword;
     Button btnRegister;
-    String emailPattern="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     ProgressDialog progressDialog;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
@@ -32,68 +33,75 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         /*Initialize the variables*/
-        inputEmail=findViewById(R.id.etMail);
-        inputPassword=findViewById(R.id.etPassword);
-        inputConfirmPasword=findViewById(R.id.etConfirmPassword);
-        btnRegister=findViewById(R.id.btAccept);
-        progressDialog=new ProgressDialog(this);
-        mAuth=FirebaseAuth.getInstance();
-        mUser=mAuth.getCurrentUser();
+        inputEmail = findViewById(R.id.etMail);
+        inputPassword = findViewById(R.id.etPassword);
+        inputConfirmPasword = findViewById(R.id.etConfirmPassword);
+        btnRegister = findViewById(R.id.btAccept);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        /*The "btnRegister" button uses a click event listener that is defined by an anonymous class, which implements the View.OnClickListener interface.
+         *The anonymous class has an onClick() method that is called when the button is clicked.
+         *Inside the onClick() method, the PerforAuth() method is called, which has the register logic.*/
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 PerforAuth();
             }
         });
 
     }
 
+    /*Method with the logic of sing up into the application*/
     private void PerforAuth() {
         String email = inputEmail.getText().toString();
-        String password=inputPassword.getText().toString();
+        String password = inputPassword.getText().toString();
         String confirmPassword = inputConfirmPasword.getText().toString();
-        
-        if(!email.matches(emailPattern)){
+        /*Check If the email does not match the pattern, an error message is displayed in the email input field.*/
+        if (!email.matches(emailPattern)) {
             inputEmail.setError("Enter conntext Email");
-            
-        } else if (password.isEmpty()||password.length()<6) {
+            /*Check if the password is valid. If the password is less than 6 characters or empty, an error message is displayed in the password input field.*/
+        } else if (password.isEmpty() || password.length() < 6) {
             inputPassword.setError("Enter Proper Password");
-            
+            /*Check if the password is equal to the entered password*/
         } else if (!password.equals(confirmPassword)) {
             inputConfirmPasword.setError("Password Not matched Both field");
-        }else {
+        } else {
             progressDialog.setMessage("Please Wait While Register...");
             progressDialog.setTitle("Register");
             progressDialog.setCanceledOnTouchOutside(false);
             progressDialog.show();
 
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                /*The createUserWithEmailAndPassword() method returns an asynchronous task that completes with an AuthResult authentication result. The addOnCompleteListener() method is used to add an event listener to this task that is called when the task completes.*/
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         progressDialog.dismiss();
                         sendUserToNextActivity();
-                        Toast.makeText(Register.this,"Registration Succesful",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Register.this, "Registration Succesful", Toast.LENGTH_SHORT).show();
 
-                    }else{
+                    } else {
                         progressDialog.dismiss();
-                        Toast.makeText(Register.this,""+task.getException(),Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Register.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
 
                     }
                 }
             });
 
 
-
         }
 
     }
 
+    /*Method to return to the login interface*/
     private void sendUserToNextActivity() {
-        Intent intent=new Intent(Register.this,MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent intent = new Intent(Register.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 
